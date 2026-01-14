d683c51bd241a55ffb6a15306c902295788dc1be
beam007-wq <huanghanbin4@gmail.com>
2026-01-14 17:05:44 +0800

    Delete ecommerce_backend directory

--- ecommerce_backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from router import admin_router,login_router,service_router,user_router
-
-app = FastAPI()
-app.mount("/auth",login_router.login_app)
-app.mount("/admin",admin_router.admin_main_app)
-app.mount("/user",user_router.user_main_app)
-app.mount("/service",service_router.service)
